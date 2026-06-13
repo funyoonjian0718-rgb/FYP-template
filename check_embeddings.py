457f62be204bc4baf88ca4c4b3ce40603dbe7d4c
Fun <funyoonjian0718@gmail.com>
2026-06-13 17:11:26 +0800

    delete

--- check_embeddings.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys, os
-sys.path.insert(0, '.')
-from app.rag.mysql_embeddings import MySQLEmbeddingService
-emb = MySQLEmbeddingService()
-count = emb.count()
-print(f'Total embeddings in MySQL: {count}')
